e92da20ab15ff3b774d20c31747cbcc89b5ffd86
Anastassios Nanos <ananos@nubificus.co.uk>
2020-12-02 17:52:12 +0000

    Initial import
    include dockerfile, scripts & example image

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DOCKER_BUILDKIT=1 docker build --network=host -f Dockerfile.guest.x86_64 -t cloudkernels/fc-guest-build --build-arg "TOKEN=$TOKEN" --target artifacts --output type=local,dest=./fc-guest-x86_64 .
+
+cd fc-guest-x86_64 && bash create_rootfs.sh rootfs/* && rm -rf rootfs
