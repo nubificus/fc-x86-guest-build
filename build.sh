48679c8ee6453b4a802fbbf3cf29dfe6d34ff338
Anastassios Nanos <ananos@nubificus.co.uk>
2020-12-03 17:55:19 +0000

    strip a script

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 DOCKER_BUILDKIT=1 docker build --network=host -f Dockerfile.guest.x86_64 -t cloudkernels/fc-guest-build --build-arg "TOKEN=$TOKEN" --target artifacts --output type=local,dest=./fc-guest-x86_64 .
 
-cd fc-guest-x86_64 && bash create_rootfs.sh rootfs/* && rm -rf rootfs
+#cd fc-guest-x86_64 && bash create_rootfs.sh rootfs/* && rm -rf rootfs
