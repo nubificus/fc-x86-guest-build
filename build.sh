fa426260da8d307bb5a26243fe193fc07dc42b56
Anastassios Nanos <ananos@nubificus.co.uk>
2021-04-10 10:22:02 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DOCKER_BUILDKIT=1 docker build --network=host -f Dockerfile.guest.x86_64 -t cloudkernels/fc-guest-build --build-arg "TOKEN=$TOKEN" --target artifacts --output type=local,dest=./fc-guest-x86_64 .
+DOCKER_BUILDKIT=1 docker build --no-cache --network=host -f Dockerfile.guest.x86_64 -t cloudkernels/fc-guest-build --build-arg "TOKEN=$TOKEN" --target artifacts --output type=local,dest=./fc-guest-x86_64 .
 
 cd fc-guest-x86_64 && bash create_rootfs.sh rootfs/* && rm -rf rootfs && rm create_rootfs.sh
